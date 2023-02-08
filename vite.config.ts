7affee1433d33b4146ddc1b3e547140d2422bbe9
chenhao <lzsoft_hh@hotmail.com>
2023-02-08 13:08:43 +0800

    alias '@' to ./src

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
-
+import path from 'path'
 import { defineConfig } from 'vite'
 import unocss from 'unocss/vite'
 import react from '@vitejs/plugin-react-swc'
@@ -8,6 +8,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), unocss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
